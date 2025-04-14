8aaea4ca1ed6672de78d8a3d0afc2a589b170bdd
ta-akb <apakbbb@gmail.com>
2025-04-14 15:03:52 +0900

    Update __init__.py

--- proms/__init__.py
+++ proms/__init__.py
@@ -1,13 +1,15 @@
-from .fs_methods import ProMS, ProMS_mo
+from .fs_methods import ProMS_so, ProMS_mo_pre, ProMS_mo_mid ,ProMS_mo_post
 from .fs_methods import fs_methods, FeatureSelector
 
 from .dataset import Dataset, Data
-__version__ = '1.0.12'
+__version__ = '1.0.0'
 __all__ = [
            'Data',
            'Dataset',
-           'ProMS',
-           'ProMS_mo',
+           'ProMS_so',
+           'ProMS_mo_pre',
+           'ProMS_mo_mid',
+           'ProMS_mo_post',
            'fs_methods',
            'FeatureSelector'
            ]
