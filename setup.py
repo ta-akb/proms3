67564115a36a09bf331d108a1592ce5063335bf6
ta-akb <apakbbb@gmail.com>
2025-04-14 15:55:22 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from setuptools import setup, find_packages
 import os
 
-__version__ = '1.0.12'
-url = 'https://github.com/bzhanglab/proms'
+__version__ = '1.0.0'
+#url = 'https://github.com/bzhanglab/proms'
 
 install_requires = [
-    'scikit-survival',
-    'numpy>=2.2.0',
-    'scipy>=1.14.1',
-    'scikit-learn',
+    'scikit-survival>=0.24.1',
+    'numpy>=2.2.4',
+    'scipy>=1.15.2',
+    'scikit-learn>=1.6.1',
     'pandas>=2.2.3',
-    'xgboost',
+    'xgboost>=3.0.0',
     'matplotlib>=3.10.0',
-    'pyyaml',
+    'pyyaml>=6.0.2',
     'seaborn>=0.13.2'
 ]
 
 
 setup(
-    name='proms',
+    name='proms3',
     version=__version__,
-    description='Protein Markers Selection',
-    author='Zhiao Shi',
-    author_email='zhiao.shi@gmail.com',
-    url=url,
-    download_url='{}/archive/{}.tar.gz'.format(url, __version__),
+    description='Markers Genes Selection',
+    author='Taiki Akiba',
+    author_email='@gmail.com',
+    #url=url,
+    #download_url='{}/archive/{}.tar.gz'.format(url, __version__),
     keywords=[
         'machine learning', 'feature selection', 'proteomics',
         'multiomics', 'biomarker', 'genomics', 'bioinformatics'
@@ -33,12 +33,18 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
-    python_requires='>=3.12.8',
+    python_requires='>=3.11',
     install_requires=install_requires,
-    packages=find_packages(),
-    entry_points={"console_scripts": ["proms_train=proms.__main__:main", 
-                  "proms_predict=proms.predict:main"]}
+    packages=find_packages(include=['proms3.*']),
+    #packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+        "proms_train3=proms.__main__:main",
+        "proms_predict3=proms.predict:main"
+        ]
+    }
+    #entry_points={"console_scripts": ["proms_train=proms.__main__:main", 
+    #              "proms_predict=proms.predict:main"]}
 )
